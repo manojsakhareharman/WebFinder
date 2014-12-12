ee9a90167a36de548442e840afc25ad791a2195e
Adam Anderson <adam@makeascene.com>
2014-12-12 15:06:38 -0700

    Splitting javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require foundation
-//= require angular/angular
-//= require angular-slider/slider
-//= require imagesloaded/imagesloaded.pkgd.min
-//= require angular-images-loaded/angular-images-loaded
 //= require app
 // require_tree .
 
---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,7 @@
+
+//= require jquery
+//= require jquery_ujs
+//= require angular/angular
+//= require angular-slider/slider
+//= require imagesloaded/imagesloaded.pkgd.min
+//= require angular-images-loaded/angular-images-loaded
