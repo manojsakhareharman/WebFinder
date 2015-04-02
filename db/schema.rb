8c68b2b96adc7685cb26298b026d7c926a8dab3e
Adam Anderson <adam@makeascene.com>
2015-04-02 13:27:18 -0600

    Rails 4.2 updates

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20141218223048) do
 
-  create_table "active_admin_comments", force: true do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace",     limit: 255
+    t.text     "body",          limit: 65535
+    t.string   "resource_id",   limit: 255,   null: false
+    t.string   "resource_type", limit: 255,   null: false
+    t.integer  "author_id",     limit: 4
+    t.string   "author_type",   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -28,17 +28,17 @@ ActiveRecord::Schema.define(version: 20141218223048) do
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
-  create_table "admin_users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admin_users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -46,36 +46,36 @@ ActiveRecord::Schema.define(version: 20141218223048) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "brands", force: true do |t|
-    t.string   "name"
-    t.string   "url"
+  create_table "brands", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "url",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "case_studies", force: true do |t|
-    t.string   "name"
-    t.string   "headline"
-    t.text     "description"
-    t.integer  "vertical_market_id"
+  create_table "case_studies", force: :cascade do |t|
+    t.string   "name",                limit: 255
+    t.string   "headline",            limit: 255
+    t.text     "description",         limit: 65535
+    t.integer  "vertical_market_id",  limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.text     "content"
-    t.string   "banner_file_name"
-    t.string   "banner_content_type"
-    t.integer  "banner_file_size"
+    t.string   "slug",                limit: 255
+    t.text     "content",             limit: 65535
+    t.string   "banner_file_name",    limit: 255
+    t.string   "banner_content_type", limit: 255
+    t.integer  "banner_file_size",    limit: 4
     t.datetime "banner_updated_at"
   end
 
   add_index "case_studies", ["slug"], name: "index_case_studies_on_slug", unique: true, using: :btree
   add_index "case_studies", ["vertical_market_id"], name: "index_case_studies_on_vertical_market_id", using: :btree
 
-  create_table "friendly_id_slugs", force: true do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",           limit: 255, null: false
+    t.integer  "sluggable_id",   limit: 4,   null: false
     t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+    t.string   "scope",          limit: 255
     t.datetime "created_at"
   end
 
@@ -84,108 +84,108 @@ ActiveRecord::Schema.define(version: 20141218223048) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
-  create_table "leads", force: true do |t|
-    t.string   "name"
-    t.string   "company"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "project_description"
-    t.string   "source"
+  create_table "leads", force: :cascade do |t|
+    t.string   "name",                limit: 255
+    t.string   "company",             limit: 255
+    t.string   "email",               limit: 255
+    t.string   "phone",               limit: 255
+    t.text     "project_description", limit: 65535
+    t.string   "source",              limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "product_types", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "slug"
+  create_table "product_types", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.string   "slug",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "product_types", ["slug"], name: "index_product_types_on_slug", unique: true, using: :btree
 
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "slug"
-    t.string   "url"
-    t.string   "photo_file_name"
-    t.string   "photo_content_type"
-    t.integer  "photo_file_size"
+  create_table "products", force: :cascade do |t|
+    t.string   "name",               limit: 255
+    t.text     "description",        limit: 65535
+    t.string   "slug",               limit: 255
+    t.string   "url",                limit: 255
+    t.string   "photo_file_name",    limit: 255
+    t.string   "photo_content_type", limit: 255
+    t.integer  "photo_file_size",    limit: 4
     t.datetime "photo_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "brand_id"
+    t.integer  "brand_id",           limit: 4
   end
 
   add_index "products", ["slug"], name: "index_products_on_slug", unique: true, using: :btree
 
-  create_table "reference_system_product_type_products", force: true do |t|
-    t.integer  "reference_system_product_type_id"
-    t.integer  "product_id"
+  create_table "reference_system_product_type_products", force: :cascade do |t|
+    t.integer  "reference_system_product_type_id", limit: 4
+    t.integer  "product_id",                       limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "reference_system_product_type_products", ["reference_system_product_type_id"], name: "r_s_p_t_id", using: :btree
 
-  create_table "reference_system_product_types", force: true do |t|
-    t.integer  "reference_system_id"
-    t.integer  "product_type_id"
-    t.integer  "quantity"
+  create_table "reference_system_product_types", force: :cascade do |t|
+    t.integer  "reference_system_id", limit: 4
+    t.integer  "product_type_id",     limit: 4
+    t.integer  "quantity",            limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "top"
-    t.string   "left"
+    t.string   "top",                 limit: 255
+    t.string   "left",                limit: 255
   end
 
   add_index "reference_system_product_types", ["reference_system_id"], name: "index_reference_system_product_types_on_reference_system_id", using: :btree
 
-  create_table "reference_systems", force: true do |t|
-    t.string   "name"
-    t.integer  "vertical_market_id"
-    t.boolean  "retail",                      default: false
-    t.integer  "position"
-    t.string   "venue_size_descriptor"
-    t.string   "headline"
-    t.text     "description"
+  create_table "reference_systems", force: :cascade do |t|
+    t.string   "name",                        limit: 255
+    t.integer  "vertical_market_id",          limit: 4
+    t.boolean  "retail",                      limit: 1,     default: false
+    t.integer  "position",                    limit: 4
+    t.string   "venue_size_descriptor",       limit: 255
+    t.string   "headline",                    limit: 255
+    t.text     "description",                 limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "banner_file_name"
-    t.string   "banner_content_type"
-    t.integer  "banner_file_size"
+    t.string   "banner_file_name",            limit: 255
+    t.string   "banner_content_type",         limit: 255
+    t.integer  "banner_file_size",            limit: 4
     t.datetime "banner_updated_at"
-    t.string   "slug"
-    t.string   "system_diagram_file_name"
-    t.string   "system_diagram_content_type"
-    t.integer  "system_diagram_file_size"
+    t.string   "slug",                        limit: 255
+    t.string   "system_diagram_file_name",    limit: 255
+    t.string   "system_diagram_content_type", limit: 255
+    t.integer  "system_diagram_file_size",    limit: 4
     t.datetime "system_diagram_updated_at"
   end
 
   add_index "reference_systems", ["slug"], name: "index_reference_systems_on_slug", unique: true, using: :btree
   add_index "reference_systems", ["vertical_market_id"], name: "index_reference_systems_on_vertical_market_id", using: :btree
 
-  create_table "site_settings", force: true do |t|
-    t.string   "name"
-    t.text     "content"
+  create_table "site_settings", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "content",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "site_settings", ["name"], name: "index_site_settings_on_name", unique: true, using: :btree
 
-  create_table "vertical_markets", force: true do |t|
-    t.string   "name"
-    t.integer  "parent_id"
+  create_table "vertical_markets", force: :cascade do |t|
+    t.string   "name",                limit: 255
+    t.integer  "parent_id",           limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "headline"
-    t.text     "description"
-    t.string   "slug"
-    t.string   "banner_file_name"
-    t.string   "banner_content_type"
-    t.integer  "banner_file_size"
+    t.string   "headline",            limit: 255
+    t.text     "description",         limit: 65535
+    t.string   "slug",                limit: 255
+    t.string   "banner_file_name",    limit: 255
+    t.string   "banner_content_type", limit: 255
+    t.integer  "banner_file_size",    limit: 4
     t.datetime "banner_updated_at"
   end
 
