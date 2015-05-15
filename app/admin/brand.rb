b3b27b8f8cf84a973d289691c41aff2d9873be07
Adam Anderson <adam@makeascene.com>
2015-05-15 12:54:10 -0600

    Adding admin fields to brands for services site

--- app/admin/brand.rb
+++ app/admin/brand.rb
@@ -1,10 +1,22 @@
 ActiveAdmin.register Brand do
-  permit_params :name, :url, :downloads_page_url, :support_url, :training_url, :logo, :white_logo, :description
+  permit_params :name,
+    :url,
+    :downloads_page_url,
+    :support_url,
+    :training_url,
+    :tech_url,
+    :logo,
+    :white_logo,
+    :description,
+    :show_on_main_site,
+    :show_on_services_site
 
   # :nocov:
   index do
     column :name
     column :url
+    column :show_on_main_site
+    column :show_on_services_site
     actions
   end
 
@@ -17,9 +29,12 @@ ActiveAdmin.register Brand do
       f.input :downloads_page_url, hint: "If present, causes this brand to appear on consultant portal page.", placeholder: "http://brand.name.com/downloads"
       f.input :support_url, hint: "If present, appears on contacts page", placeholder: "http://brand.name.com/support"
       f.input :training_url, hint: "If present, appears on the training page.", placeholder: "http://brand.name.com/training"
+      f.input :tech_url, hint: "Appears on the service center login page if present.", placeholder: "http://tech.brand.com"
       f.input :logo
       f.input :white_logo
       f.input :description
+      f.input :show_on_main_site
+      f.input :show_on_services_site
     end
     f.actions
   end
