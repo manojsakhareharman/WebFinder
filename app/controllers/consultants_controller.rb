39f3ed9b66357743bc324f5bee3008eaf0c52628
Adam Anderson <adam@makeascene.com>
2015-06-10 11:16:58 -0600

    Re-sorting consultant contact brands, new service image

--- app/controllers/consultants_controller.rb
+++ app/controllers/consultants_controller.rb
@@ -2,7 +2,9 @@ class ConsultantsController < ApplicationController
 
   def index
     @brands = Brand.for_consultant_portal
-    @brands_with_contact_info = @brands.where("contact_info_for_consultants IS NOT NULL and contact_info_for_consultants != ''")
+    all_brands_with_contact_info = @brands.where("contact_info_for_consultants IS NOT NULL and contact_info_for_consultants != ''")
+    first_brands_with_contact_info = all_brands_with_contact_info.where("name LIKE 'Harman%%'")
+    @brands_with_contact_info = first_brands_with_contact_info + (all_brands_with_contact_info - first_brands_with_contact_info)
   end
 
 end
