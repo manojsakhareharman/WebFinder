8a74e47751c731265eca160a254ac54ec37768b1
Adam Anderson <adam.anderson@harman.com>
2016-02-23 14:45:32 -0700

    Morphing closer to Harman theme

--- spec/views/shared/_learn_plan_buy_nav.html.erb_spec.rb
+++ spec/views/shared/_learn_plan_buy_nav.html.erb_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "shared/_learn_plan_buy_nav.html.erb", :type => :view do
-
-  before do
-    render partial: "shared/learn_plan_buy_nav"
-  end
-
-  it "should have links to Learn, Plan, Buy" do
-    expect(rendered).to have_link "Learn"
-    expect(rendered).to have_link "Plan"
-    expect(rendered).to have_link "Buy"
-  end
-
-  context "magellan (zurb foundation)" do
-    it "should have magellan expedition container" do
-      expect(rendered).to have_xpath("//div[@data-magellan-expedition='fixed']")
-    end
-
-    it "should have magellan arrival links" do
-      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='learn']")
-      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='plan']")
-      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='buy']")
-    end
-
-  end
+# 2016-02-23 AA removed the learn/plan/buy subnav
+# (commented out the HTML code in the partial)
+#  before do
+#    render partial: "shared/learn_plan_buy_nav"
+#  end
+#
+#  it "should have links to Learn, Plan, Buy" do
+#    expect(rendered).to have_link "Learn"
+#    expect(rendered).to have_link "Plan"
+#    expect(rendered).to have_link "Buy"
+#  end
+#
+#  context "magellan (zurb foundation)" do
+#    it "should have magellan expedition container" do
+#      expect(rendered).to have_xpath("//div[@data-magellan-expedition='fixed']")
+#    end
+#
+#    it "should have magellan arrival links" do
+#      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='learn']")
+#      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='plan']")
+#      expect(rendered).to have_xpath("//dd[@data-magellan-arrival='buy']")
+#    end
+#
+#  end
 end
