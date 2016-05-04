b29871d8a41d070598b04303b063cd8b6e0f19d1
Adam Anderson <adam.anderson@harman.com>
2016-05-04 07:51:56 -0600

    Slight syntax change for globalize initializer

--- config/initializers/globalize.rb
+++ config/initializers/globalize.rb
@@ -1,11 +1,11 @@
 Globalize.fallbacks = {
-  en: [:en, :'en-US', :'en-GB'],
-  'en-US': [:'en-US', :en, :'en-GB'],
-  'en-GB': [:'en-GB', :en, :'en-US'],
-  es: [:es, :'es-MX', :en],
-  'es-MX': [:'es-MX', :es, :en],
-  zh: [:zh, :en],
-  cn: [:zh, :en]
+  :en => [:en, :'en-US', :'en-GB'],
+  :'en-US' => [:'en-US', :en, :'en-GB'],
+  :'en-GB' => [:'en-GB', :en, :'en-US'],
+  :es => [:es, :'es-MX', :en],
+  :'es-MX' => [:'es-MX', :es, :en],
+  :zh => [:zh, :en],
+  :cn => [:zh, :en]
 }
 
 # My monkey patching
