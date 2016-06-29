52947810651b49e37e45d4a0e7ac9b6dcfc223aa
Adam Anderson <adam.anderson@harman.com>
2016-06-29 11:05:39 -0600

    Adding sandbox environment

---
+++ config/deploy/sandbox.rb
@@ -0,0 +1,55 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+
+server 'rackspace1', user: 'hmg', roles: %w{web app db}
+
+set :application, "harmanpro_sandbox"
+set :deploy_to, "/var/www/harmanpro-sandbox"
+
+#ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git symbolic-ref --short HEAD`.chomp
+
+#namespace :deploy do
+#
+#  before :restart, :refresh_node2_uploads do
+#    invoke "refresh:node2_uploads"
+#  end
+#
+#end
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
